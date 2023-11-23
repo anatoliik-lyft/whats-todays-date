2fac100cb748d35765f0f0c5449192de10e120b6
anatoliik-lyft <anatoliik@lyft.com>
2023-11-23 02:22:26 -0800

    Remove ;, fix scroll bar on mobile devices

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import { appWithTranslation } from "next-i18next";
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
       <Analytics />
     </>
   );
