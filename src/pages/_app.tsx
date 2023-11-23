64d36a497e3f15144d99d58cc5cb56c277be1770
anatoliik-lyft <anatoliik@lyft.com>
2023-11-23 02:04:00 -0800

    Add  @vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
@@ -5,7 +7,12 @@ import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 };
 
 export default appWithTranslation(App);
